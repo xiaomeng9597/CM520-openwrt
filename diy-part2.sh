b24b3aa826dfb053a785bca87dc0cc0eb66b7202
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-10-21 07:33:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,12 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
-
-rm -rf package/lean/luci-theme-argon
-rm -rf themes/luci-theme-argon
-
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
