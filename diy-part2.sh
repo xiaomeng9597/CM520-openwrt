fef73d6e439817d71721ec3bb754063c66642cb1
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-10-20 14:06:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,6 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 
 rm -rf package/lean/luci-theme-argon
-
+rm -rf themes/luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
