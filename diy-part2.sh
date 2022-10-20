aecab867590c3eb084b4a20fd90360a40d879d95
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-10-20 10:04:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/Luci-theme-argon
