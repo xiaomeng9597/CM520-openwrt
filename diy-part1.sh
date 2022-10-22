97c892597380ca6207ecb92ce4a9d24d2eae146e
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-10-22 14:20:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@ rm -rf themes/luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-git clone https://github.com/xiaomeng9597/small-package package/small-package
+git clone https://github.com/xiaomeng9597/small-package.git package/small-package
 
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
