b53699a6e18a09f5740cc17ad19e8e8f8135bc53
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-10-22 09:10:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,3 +29,6 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Ma
 
 sed -i '$a src-git kenzo https://github.com/xiaomeng9597/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
