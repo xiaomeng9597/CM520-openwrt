6fb5e28c10b85070ad9551e88867511dd35896ae
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-10-27 12:00:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,13 +13,3 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-
-
-
-# 修改所需的文件
-sed -i 's/luci-app-samba4/luci-app-samba/g' package/lean/autosamba/Makefile
-sed -i 's/luci-app-unblockmusic//g' target/linux/ipq40xx/Makefile
-sed -i 's/CONFIG_FAT_DEFAULT_IOCHARSET=\"iso8859-1\"/CONFIG_FAT_DEFAULT_IOCHARSET=\"utf8\"/g' target/linux/generic/config-6.0
-sed -i 's/CONFIG_FAT_DEFAULT_IOCHARSET=\"iso8859-1\"/CONFIG_FAT_DEFAULT_IOCHARSET=\"utf8\"/g' target/linux/generic/config-5.4
-sed -i 's/CONFIG_FAT_DEFAULT_IOCHARSET=\"iso8859-1\"/CONFIG_FAT_DEFAULT_IOCHARSET=\"utf8\"/g' target/linux/generic/config-5.15
-sed -i 's/CONFIG_FAT_DEFAULT_IOCHARSET=\"iso8859-1\"/CONFIG_FAT_DEFAULT_IOCHARSET=\"utf8\"/g' target/linux/generic/config-5.10
