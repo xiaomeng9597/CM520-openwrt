ab941d0a2ef697ea1b3e7f1b00082daeddbaf400
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-10-31 06:03:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,6 +28,6 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 
 
-#sed -i 's/coolsnowwolf\/luci/xiaomeng9597\/luci/g' feeds.conf.default
+sed -i 's/coolsnowwolf\/luci/xiaomeng9597\/luci/g' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/xiaomeng9597/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
